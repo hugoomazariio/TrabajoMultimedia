8f265a7c9ba0d8e7a917cbd665bfcc8d3d9d9e41
Hugo <160237140+hugoomazariio@users.noreply.github.com>
2026-04-05 15:05:41 +0200

    Ruta repositorio github y transición expedientes

--- recursos_Multimedia/JS/cambioExpedientePersonaje.js
+++ recursos_Multimedia/JS/cambioExpedientePersonaje.js
@@ -3,20 +3,35 @@ let btnPJ2 = document.getElementById("botonPJ2");
 let btnPJ3 = document.getElementById("botonPJ3");
 let btnSalir = document.getElementById("salir");
 let imgchange = document.getElementById("imgchange");
+let fadeInContainer = imgchange.parentElement; // Obtener el contenedor .fade-in
+
+// Función para cambiar imagen con transición fade
+function cambiarImagenConFade(nuevaRuta) {
+    // Agregar la animación de fade-out
+    fadeInContainer.classList.add("fade-out");
+    
+    // Después de que termine el fade-out, cambiar la imagen y hacer fade-in
+    setTimeout(function() {
+        fadeInContainer.classList.remove("fade-out");
+        imgchange.src = nuevaRuta;
+        fadeInContainer.classList.add("fade-in");
+    }, 800); // 800ms coincide con la duración de la animación
+}
 
 btnPJ1.onclick = function() {
-    imgchange.src = "recursos_Multimedia/img/Personajes_Cazarrecompensas.jpg";
+    cambiarImagenConFade("recursos_Multimedia/img/Personajes_Cazarrecompensas.jpg");
 }
 
 btnPJ2.onclick = function() {
-    imgchange.src = "recursos_Multimedia/img/Personajes_Sheriff.jpg";
+    cambiarImagenConFade("recursos_Multimedia/img/Personajes_Sheriff.jpg");
 }
 
 btnPJ3.onclick = function() {
-    imgchange.src = "recursos_Multimedia/img/instagram-icon.png";
+    cambiarImagenConFade("recursos_Multimedia/img/instagram-icon.png");
 }
+
 btnSalir.onclick = function() {
-    imgchange.src = "recursos_Multimedia/img/Personajes_ExpedienteCerrado.jpg";
+    cambiarImagenConFade("recursos_Multimedia/img/Personajes_ExpedienteCerrado.jpg");
 }
 
 
